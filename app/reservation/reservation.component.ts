cc2a9eb46ec3ec720ae431d5969c2fdffe9d75f2
Usman Akram <usman.akram1996@outlook.com>
2018-03-03 23:59:10 +0500

    Assignment 3 Task 1 Initial

--- app/reservation/reservation.component.ts
+++ app/reservation/reservation.component.ts
@@ -6,6 +6,13 @@ import {Validators, FormBuilder, FormGroup} from '@angular/forms';
 import {ModalDialogService, ModalDialogOptions} from 'nativescript-angular/modal-dialog';
 import {ReservationModalComponent} from '../reservationmodal/reservationmodal.component';
 
+import { Animation,AnimationDefinition } from 'ui/animation';
+import {Page} from 'ui/page';
+import * as enums from 'ui/enums';
+import {View} from 'ui/core/view';
+
+
+
 @Component({
     selector:'app-reservation',
     moduleId:module.id,
@@ -16,10 +23,14 @@ import {ReservationModalComponent} from '../reservationmodal/reservationmodal.co
 export class ReservationComponent extends DrawerPage implements OnInit{
 
     reservation: FormGroup; // used to store reactive form
-
+    formReserve:View;
+    userData:View;
+    formSubmit:boolean = false;
+    
     constructor(private changeDetectorReg:ChangeDetectorRef,
         private modalService:ModalDialogService,
         private vcRef: ViewContainerRef,
+        private page:Page,
         private formBuilder:FormBuilder){
             super(changeDetectorReg);
             
@@ -31,7 +42,8 @@ export class ReservationComponent extends DrawerPage implements OnInit{
     }
 
     ngOnInit(){
-
+        this.userData = this.page.getViewById<View>("userData");
+        this.userData.visibility = "hidden";
     }
 
 
@@ -75,7 +87,36 @@ export class ReservationComponent extends DrawerPage implements OnInit{
     }
 
     onSubmit(){
+        
+        this.formSubmit = true;
+        this.formReserve = this.page.getViewById<View>("formReserve");
+        this.formReserve.animate({
+         
+            translate:{x:0, y:+200 },
+            opacity:0,
+            duration:700,
+            curve:enums.AnimationCurve.easeOut
+        }); 
+
+        this.userData = this.page.getViewById<View>("userData");      
+        
+        this.userData.visibility ="visible";
+        this.userData.animate({
+            translate:{x:0, y:+200 },
+            opacity:1,
+            duration:500,
+            curve:enums.AnimationCurve.easeIn
+        });
+
         console.log(JSON.stringify(this.reservation.value));
+        
     }
 
+    // showView(){
+    //     this.formSubmit = true;
+        
+    // }
+
+
+  
 }
